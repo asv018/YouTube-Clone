487b8343dc3992edbf9c79119e5bca679443efec
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-08-01 21:13:27 +0530

    Done

--- youtube/src/components/Feed.jsx
+++ youtube/src/components/Feed.jsx
@@ -20,9 +20,9 @@ const Feed = () => {
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+        {/* <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2023 JSM Media
-        </Typography>
+        </Typography> */}
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
